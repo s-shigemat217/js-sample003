6aa7b20fa08ee1ba819779b6e4ed0e57c5b016c7
sgmt <s.shigematsu17@gmail.com>
2025-07-01 11:42:32 +0900

    フォトビューワーの初期化処理を修正し、画像リストを追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,63 @@
 class photoViewer {
+
+  constructor(rootElm, images) {
+    this.rootElm = rootElm;
+    this.images = images;
+    this.currentIndex = 0;
+    
+  }
   // 初期化
   init(){
-    const rootElm = document.getElementById('photoViewer');
-    const frameElm = rootElm.querySelector('.frame');
-    const image = "https://picsum.photos/600/400";
+    const nextButton = this.rootElm.querySelector('.nextButton');
+    nextButton.addEventListener('click', () => {
+      this.next();
+    });
 
+    const prevButton = this.rootElm.querySelector('.prevButton');
+    prevButton.addEventListener('click', () => {
+      this.prev();
+    });
+
+    this.updatePhoto();
+  }
+
+  updatePhoto(){
+    const frameElm = this.rootElm.querySelector('.frame');
+    const image = this.images[this.currentIndex];
     frameElm.innerHTML = `
       <div class="currentImage">
         <img src="${image}" alt="Random Image">
       </div>
-      `;
+    `;
+  };
+
+  next() {
+    const lastIndex = this.images.length - 1;
+    if (this.currentIndex >= lastIndex) {
+      this.currentIndex = 0; // 最後の画像の次は最初の画像に戻る
+    } else {
+      this.currentIndex++;
+    }
+    this.updatePhoto();
+  }
+
+  prev() {
+    const lastIndex = this.images.length - 1;
+    if (this.currentIndex <= 0) {
+      this.currentIndex = lastIndex; // 最初の画像の前は最後の画像に戻る
+    } else {
+      this.currentIndex--;
+    }
+    this.updatePhoto();
   }
 }
 
-new photoViewer().init();
+const images = [
+  'https://picsum.photos/800/600?random=1',
+  'https://picsum.photos/800/600?random=2',
+  'https://picsum.photos/800/600?random=3',
+  'https://picsum.photos/800/600?random=4',
+  'https://picsum.photos/800/600?random=5'
+];
+
+new photoViewer(document.getElementById('photoViewer'), images).init();
